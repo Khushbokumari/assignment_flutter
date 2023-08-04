1b616129daffe0832133d0cfaa98bd055ca130f5
khushboo kumari <kumari.khushboo398@gmail.com>
2023-08-04 11:19:32 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+import 'Screens/home.dart';
+import 'Screens/login.dart';
+import 'Screens/verify.dart';
+
+
+void main() async {
+  // Initialize Firebase before running the app
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Welcome(),
+      routes: {
+        'phone': (context) => MyPhone(),
+        'verify': (context) => MyVerify()
+      },
+    );
+  }
+}
